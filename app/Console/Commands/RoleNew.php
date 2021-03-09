08d991f33f075fbbc8ecc1995f9b639732c2232f
= <=>
2021-03-09 20:49:34 +0200

    Make changes for deployment

---
+++ app/Console/Commands/RoleNew.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Role;
+
+class RoleNew extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'role:new';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new role';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        return Role::create([
+            'title' =>  $this->ask('Role Title:'),
+        ]);
+    }
+}
