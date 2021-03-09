2fe60885583d7390f389821ffbfa76fa2f7a7476
= <=>
2021-03-09 21:53:33 +0200

    Make changes for deployment

--- app/Category.php
+++ app/Category.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = [
+        'name'
+    ];
     
     //Return all posts associated with a curtain category
     public function posts(){
